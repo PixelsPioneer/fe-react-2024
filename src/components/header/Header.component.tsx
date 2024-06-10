6719458991fec680a7cd243c535b4e3eaa3c877e
Andrii Voloshchuk <andriyvoloshyk228@gmail.com>
2024-06-10 10:07:07 +0300

    rafactor: refactored all styles for better adaptive pages

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -30,50 +30,58 @@ export const HeaderComponent: React.FC<HeaderComponentProps> = ({ toggleComponen
     }, [selectedProducts]);
 
     return (
-        <header className={styles.header}>
-            <div className={styles.themesContainer}>
-                <img className={styles.logoMOCG} src={LogoMOCG} alt="Logo MA" />
-                <div className={styles.themesMenuContainer}>
-                    <button className={`${styles.lightThemeButton} ${isDarkTheme ? '' : styles.active}`} onClick={() => setTheme('light')}>
-                        <img className={styles.lightThemeIcon} src={LightTheme} alt="Light Theme" />
-                    </button>
-                    <hr className={styles.customHr} />
-                    <button className={`${styles.darkThemesButton} ${isDarkTheme ? styles.active : ''}`} onClick={() => setTheme('dark')}>
-                        <img className={styles.darkThemeIcon} src={DarkTheme} alt="Dark Theme" />
-                    </button>
+        <div className={styles.header_wrapper}>
+            <header className={styles.header}>
+                <div className={styles.themesContainer}>
+                    <img className={styles.logoMOCG} src={LogoMOCG} alt="Logo MA" />
+                    <div className={styles.themesMenuContainer}>
+                        <button
+                            className={`${styles.lightThemeButton} ${isDarkTheme ? '' : styles.active}`}
+                            onClick={() => setTheme('light')}
+                        >
+                            <img className={styles.lightThemeIcon} src={LightTheme} alt="Light Theme" />
+                        </button>
+                        <hr className={styles.customHr} />
+                        <button
+                            className={`${styles.darkThemesButton} ${isDarkTheme ? styles.active : ''}`}
+                            onClick={() => setTheme('dark')}
+                        >
+                            <img className={styles.darkThemeIcon} src={DarkTheme} alt="Dark Theme" />
+                        </button>
+                    </div>
                 </div>
-            </div>
-            <div className={styles.menuAndAuthContainer}>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.burgerMenuButton}>
-                        <img className={styles.burgerMenu} src={BurgerMenu} alt="Burger Menu" />
-                    </button>
-                </div>
-                <div className={styles.aboutProdct}>
-                    <button className={styles.aboutMe} onClick={() => toggleComponent('About')}>
-                        About
-                    </button>
+                <div className={styles.menuAndAuthContainer}>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.burgerMenuButton}>
+                            <img className={styles.burgerMenu} src={BurgerMenu} alt="Burger Menu" />
+                        </button>
+                    </div>
+                    <div className={styles.aboutProdct}>
+                        <button className={styles.aboutMe} onClick={() => toggleComponent('About')}>
+                            About
+                        </button>
 
-                    <button className={styles.products} onClick={() => toggleComponent('Products')}>
-                        Products
-                    </button>
+                        <button className={styles.products} onClick={() => toggleComponent('Products')}>
+                            Products
+                        </button>
+                    </div>
+                    <div className={styles.buttonContainer}>
+                        <button className={styles.Card}>
+                            <img className={styles.cardIcon} src={Card} alt="Card" />
+                            {totalItemsInCart > 0 && <span className={styles.cartIndicator}>{totalItemsInCart}</span>}
+                        </button>
+                        <button className={styles.logOutButton}>
+                            <img className={styles.loginLogo} src={Login} alt="Login" />
+                            <p>Login</p>
+                        </button>
+                        <button className={styles.signupButton}>
+                            <img className={styles.signupLogo} src={Signup} alt="Signup" />
+                            <p>Sign up</p>
+                        </button>
+                    </div>
                 </div>
-                <div className={styles.buttonContainer}>
-                    <button className={styles.Card}>
-                        <img className={styles.cardIcon} src={Card} alt="Card" />
-                        {totalItemsInCart > 0 && <span className={styles.cartIndicator}>{totalItemsInCart}</span>}
-                    </button>
-                    <button className={styles.logOutButton}>
-                        <img className={styles.loginLogo} src={Login} alt="Login" />
-                        <p>Login</p>
-                    </button>
-                    <button className={styles.signupButton}>
-                        <img className={styles.signupLogo} src={Signup} alt="Signup" />
-                        <p>Sign up</p>
-                    </button>
-                </div>
-            </div>
-        </header>
+            </header>
+        </div>
     );
 };
 
